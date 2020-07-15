1ca22451902bf6dfd929ac3222245e30ccd7383c
Milad Nazari <contact@miladnazari.be>
2020-07-15 14:30:58 +0200

    Restructure project into models, viewmodels, views and services modules. Add email validation for login of aircrew

---
+++ frontend/Ifes/Ifes/ViewModels/Aircrew/LoginViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifes.ViewModels.Aircrew
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (!IsValidEmail(value))
+                {
+                    throw new Exception("E-mail is not valid.");
+                }
+                else if (value != _email)
+                {
+                    _email = value;
+                    NotifyPropertyChanged("Email");
+                }
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value != _password)
+                {
+                    _password = value;
+                    NotifyPropertyChanged("Password");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- frontend/Ifes/Ifes/Views/Aircrew/Login.xaml.cs
+++ frontend/Ifes/Ifes/Views/Aircrew/Login.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Ifes.ViewModels.Aircrew;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,6 +19,8 @@ namespace Ifes.Aircrew
 {
     public sealed partial class Login : Page
     {
+        private LoginViewModel _viewModel;
+
         public Login()
         {
             this.InitializeComponent();
@@ -30,6 +34,8 @@ namespace Ifes.Aircrew
             this.KeyboardAccelerators.Add(AltLeft);
             // ALT routes here
             AltLeft.Modifiers = Windows.System.VirtualKeyModifiers.Menu;
+
+            this._viewModel = new LoginViewModel();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -62,6 +68,27 @@ namespace Ifes.Aircrew
         private void OnBtnLogin(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Aircrew.Dashboard), null);
+        }
+
+        private void OnChangeTextEmail(object sender, TextChangedEventArgs e)
+        {
+            ValidateLoginForm(TextBoxEmail.Text, PasswordBoxPassword.Password.ToString());
+        }
+
+        private void ValidateLoginForm(string email, string password)
+        {
+            try
+            {
+                this._viewModel.Email = email;
+                this._viewModel.Password = password;
+                TextBoxEmail.BorderBrush = new SolidColorBrush(Colors.White);
+                BtnLogin.IsEnabled = true;
+            }
+            catch (Exception)
+            {
+                TextBoxEmail.BorderBrush = new SolidColorBrush(Colors.Red);
+                BtnLogin.IsEnabled = false;
+            }
         }
     }
 }
