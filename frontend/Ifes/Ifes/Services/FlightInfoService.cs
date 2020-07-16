ce53e57a87c58ef8cccf8a6a1d7ba71ea450962e
Milad Nazari <contact@miladnazari.be>
2020-07-16 16:59:18 +0200

    Add empty singleton services for flight information and passenger management

---
+++ frontend/Ifes/Ifes/Services/FlightInfoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifes.Services
+{
+    public sealed class FlightInfoService
+    {
+        private static readonly Lazy<FlightInfoService> lazy = new Lazy<FlightInfoService>(() => new FlightInfoService());
+
+        public static FlightInfoService Instance { get { return lazy.Value; } }
+
+        private FlightInfoService()
+        {
+        }
+    }
+}
---
+++ frontend/Ifes/Ifes/Services/PassengersService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifes.Services
+{
+    public sealed class PassengersService
+    {
+        private static readonly Lazy<PassengersService> lazy = new Lazy<PassengersService>(() => new PassengersService());
+
+        public static PassengersService Instance { get { return lazy.Value; } }
+
+        private PassengersService()
+        {
+        }
+    }
+}
