b323af7c5d48fa5fcbcba5d77922b73bc8831805
Vincent Vandewyer <vincent.vandewyer@student.hogent.be>
2020-07-31 13:00:11 +0200

    data init

--- backend/ifes/ifes/Startup.cs
+++ backend/ifes/ifes/Startup.cs
@@ -35,10 +35,11 @@ namespace ifes {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Init init) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
+            init.Run();
 
             app.UseHttpsRedirection();
 
